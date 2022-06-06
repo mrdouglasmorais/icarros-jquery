97bdb859f8e05b795650f18c4ceb94ab7d9cd9ae
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-06-06 19:56:20 -0300

    update jquery and HTML

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('button').click(function(){
+    // $('#test').hide();
+
+    // ajax api
+    // $.ajax({
+    //   url: 'https://webhook.site/a1857554-92e4-4317-a949-9712b910ba4b',
+    //   type: 'POST',
+    //   data: {
+    //     name: 'Douglas'
+    //   },
+    //   contentType: 'application/json',
+    //   success: function() {
+    //     console.log('Sucesso!')
+    //   }
+    // })
+
+    // adicionando classe
+    // $('p').addClass('red')
+
+    // removendo classe
+    // $('p').removeClass('red')
+  })
+})
