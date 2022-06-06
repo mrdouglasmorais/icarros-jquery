9c08ebe02b81b8a63a1a252fe24e92236355e43e
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-06-06 20:17:59 -0300

    update jquery and HTML

--- script.js
+++ script.js
@@ -19,6 +19,25 @@ $(document).ready(function(){
     // $('p').addClass('red')
 
     // removendo classe
-    // $('p').removeClass('red')
+    $('p').removeClass('red')
+  })
+  $('#formulario').submit(function(e){
+    e.preventDefault();
+    let data = {
+      name: $('#name').val(),
+      phone: $('#phone').val(),
+      email: $('#email').val(),
+    }
+    let badyContent = JSON.stringify(data)
+    $.ajax({
+      url: 'https://webhook.site/a1857554-92e4-4317-a949-9712b910ba4b',
+      type: 'POST',
+      data: badyContent,
+      contentType: 'application/json',
+      success: function(){
+        alert('Enviado')
+      }
+    })
+
   })
 })
